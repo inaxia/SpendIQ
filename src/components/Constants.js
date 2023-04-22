958186fdc46eef23b82d1b7751b28104f00a355e
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-04-22 19:59:58 +0530

    feat/constants

---
+++ src/components/Constants.js
@@ -0,0 +1,8 @@
+// Sidebar Icons
+import DashboardIcon from '@mui/icons-material/Dashboard';  // Dashboard Icon
+import SavingsIcon from '@mui/icons-material/Savings'; // Finance Tracker
+
+export const sidebarList = [
+    { name: "Dashboard", link: "/pages/dashboard", icon: <DashboardIcon /> },
+    { name: "Finance Tracker", link: "/pages/financeTracker", icon: <SavingsIcon /> },
+];
